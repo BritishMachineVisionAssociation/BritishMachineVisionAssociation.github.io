084fa50dd84dc1996636f02ca0fb9915b46a3d39
Neill D.F. Campbell <neill.campbell@cantab.net>
2023-07-12 16:51:34 +0100

    Updated PDF links to archive for old BMVC.

--- bmvc/scripts/sed_archive_script.sh
+++ bmvc/scripts/sed_archive_script.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-for f in *.html; 
-do 
-	sed -i '' 's|href="\(.*\.pdf\)"|href="https://bmva-archive.org.uk/bmvc/1987/\1"|g' "$f"; 
-done
+# for f in *.html; 
+# do 
+# 	sed -i '' 's|href="\(.*\.pdf\)"|href="https://bmva-archive.org.uk/bmvc/1987/\1"|g' "$f"; 
+# done
 
+for f in */index.md;
+do
+	g="$(dirname $f)"
+	echo "$g"
+
+	for h in $g/*.html;
+	do
+		echo $h
+
+		sed -i '' 's;href="\(.*\.pdf\)";href="https:\/\/bmva-archive.org.uk\/bmvc\/'$g'\/\1";g' $h
+	done
+done
